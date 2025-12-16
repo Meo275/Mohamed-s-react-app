e8667367fafc61e345dff3485707ccd791713d54
Mohamed Bihi <mohamedabdi2037@gmail.com>
2025-12-16 01:11:29 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Greeting from './components/Greeting'
+import Header from './components/Header'
+function App() {
+  return (
+    <>
+    <Greeting name="Mohamed" surname= "Abdi" />
+    <Header />
+    <Header />
+    <Header />
+    <Header />
+
+
+    </>
+  )
+}
+
+export default App
